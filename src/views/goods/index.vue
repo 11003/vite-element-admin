6a284af00cd30c70ac99e34bd3c78814fc8bdc46
Sonder <814029073@qq.com>
2023-07-14 17:52:07 +0800

    edit

--- src/views/goods/index.vue
+++ src/views/goods/index.vue
@@ -1,10 +1,41 @@
 <template>
   <div>
-   123
+    <div id="dplayer"></div>
   </div>
 </template>
 
 <script setup>
+import {onMounted} from "vue";
+import DPlayer from 'dplayer';
+onMounted(()=>{
+  const options = {
+    container: document.getElementById('dplayer'),
+    theme: '#FADFA3',
+    video: {
+      pic: 'https://p.qpic.cn/qqgameedu/0/f52a436b3bc38e0e05fe5a1b9a1e6290_0/0',
+      type: 'auto',
+      url: 'https://www.artplayer.org/assets/sample/video.mp4',
+    },
+    contextmenu: [
+      {
+        text: '关于小橘子',
+        click: (player) => {
+          console.log(player);
+        },
+      },
+    ],
+    playbackSpeed: [0.5, 0.75, 1, 1.25, 1.5, 2, 3],
+  }
+  const dp = new DPlayer(options);
+})
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+#dplayer {
+  width: 80%;
+  height: 980px;
+}
+.dplayer-menu-item:not(:first-child){
+  display: none;
+}
+</style>
--- src/views/params/index.vue
+++ src/views/params/index.vue
@@ -1,7 +1,43 @@
 <template>
-  <div>params</div>
+  <div>
+    <div class="artplayer-app"></div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import {onMounted} from "vue";
+import Artplayer from 'Artplayer';
+onMounted(()=>{
+  var art = new Artplayer({
+    container: '.artplayer-app',
+    url: 'https://www.artplayer.org/assets/sample/video.mp4',
+    poster: 'https://p.qpic.cn/qqgameedu/0/f52a436b3bc38e0e05fe5a1b9a1e6290_0/0',
+    setting: true,
+    flip: true,
+    playbackRate: true,
+    aspectRatio: true,
+    subtitleOffset: true,
+    fullscreen: true,
+    fullscreenWeb: true,
+    theme: '#ffad00',
+    hotkey: true,
+    pip: true,
+    miniProgressBar: true,
+    fastForward: true,
+    autoOrientation: true,
+    thumbnails: {
+      url: 'https://www.artplayer.org/assets/sample/thumbnails.png',
+      number: 60,
+      column: 10,
+    },
+  });
+})
 
-<style lang="scss" scoped></style>
+</script>
+
+<style lang="scss" scoped>
+.artplayer-app {
+  width: 80%;
+  height: 980px;
+}
+</style>
